44418c0dff2d0cd9717631af8f18fe836e95298e
choiizoe <choiizoe127@gmail.com>
2025-11-06 14:37:54 +0000

    feat: implement search and registration functionality with corresponding views

--- routes/main.js
+++ routes/main.js
@@ -21,5 +21,18 @@ router.get("/search", (req, res) => {
     res.render("search.ejs", shopData)
 });
 
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+});
+
+router.get("/register", (req, res) => {
+    res.render("register.ejs", shopData); 
+}); 
+ 
+router.post("/registered", (req, res) => { 
+  res.send('Hello '+ req.body.first + ' '+ req.body.last + '. You are now registered! We will send an email to: ' + req.body.email);   
+});
+
 // Export the router object so index.js can access it
 module.exports = router;
